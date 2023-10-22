118daf10cee773e0722a810162b8d4d3502e0d7a
ignaciodebat <112862961+IgnacioDebat@users.noreply.github.com>
2023-10-22 18:08:07 -0300

    dappData update

--- src/lib/config/dappData.ts
+++ src/lib/config/dappData.ts
@@ -1,6 +1,7 @@
 const dappData = {
-	title: 'Emerald City Boilerplate',
-	description: 'SvelteKit Boilerplate for creating Flow DApps',
+	title: 'Fungible Token',
+	description:
+		'A DApp that lets users create their own fungible token and transfer them to another account on Flow testnet.',
 	url: 'your-dapp-url.com',
 	author: 'Emerald City DAO',
 	icon: 'https://i.imgur.com/KSL4gq0.png'
